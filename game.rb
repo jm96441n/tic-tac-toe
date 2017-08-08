797dc30af26989057c1124869481f8cc0fb178b0
John Maguire <john.maguire@tenavox.com>
2017-08-08 09:20:47 -0500

    added initialization for current_player

--- game.rb
+++ game.rb
@@ -1,12 +1,13 @@
 require_relative './board'
 
 class Game
-  attr_accessor :board
+  attr_accessor :board, :current_player
   attr_reader   :computer, :user
 
   def initialize computer, user
-    @board    = Board.new
-    @computer = computer
-    @user     = user
+    @board          = Board.new
+    @computer       = computer
+    @user           = user
+    @current_player = @user.letter == 'X' ? @user : @computer
   end
 end
