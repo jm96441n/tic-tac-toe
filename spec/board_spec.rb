7780ff06a73e1619d8095cabdf43fa6cc73c4db0
John Maguire <john.maguire@tenavox.com>
2017-08-07 21:24:17 -0500

    added test to ensure that a taken position cannot be overwritten

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,7 +8,14 @@ describe Board do
   end
 
   it 'allows a player to take a position' do
-    board.take_position 'X', "0,1"
+    board.take_position 'X', '0,1'
+
+    expect(board.board[0][1]).to eql('X')
+  end
+
+  it 'does not allow a player to take a position that is already taken' do
+    board.take_position 'X', '0,1'
+    board.take_position 'O', '0,1'
 
     expect(board.board[0][1]).to eql('X')
   end
